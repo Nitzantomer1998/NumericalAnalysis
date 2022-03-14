f999423291559295eb8309a8f84461585135047c
Nitzantomer1998 <nitzantomer1998@gmail.com>
2022-03-14 10:05:05 +0200

    Adding Print Function

---
+++ Assignment_Two/main.py
@@ -0,0 +1,11 @@
+# Author : Nitzan Tomer.
+# Assignment Number Two.
+
+def print_matrix(matrix):
+    """
+    Matrix Printing Function
+
+    :param matrix: Matrix NxM
+    """
+    for row in range(len(matrix)):
+        print(matrix[row])
