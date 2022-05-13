0b4953d23940d5f84ad077e71174019cc7ba9903
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-05-13 08:19:04 +0300

    Adding new helpful prints

--- LinearEquation/IterativeMethods/Jacobi.py
+++ LinearEquation/IterativeMethods/Jacobi.py
@@ -126,6 +126,7 @@ def JacobiMethod():
 
             # Saving the Linear Equation final solution
             printIntoFile(currentIteration, None, True, True)
+            print('[Linear Equation Solution]\n' + str(list(map(lambda x: int(x[0] * 10 ** 5) / 10 ** 5, currentIteration))))
 
         # According message In case there is more or less than one solution
         else:
