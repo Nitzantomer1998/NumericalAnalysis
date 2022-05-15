40ae2f0178b897b73a93b85504b58696950c5ff6
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-05-15 12:34:40 +0300

    Adding maximum running time

--- Roots/IterativeMethods/Bisection.py
+++ Roots/IterativeMethods/Bisection.py
@@ -138,6 +138,8 @@ if __name__ == "__main__":
     domainStart = -3
     domainEnd = 2
 
+     # Variable to store the maximum iteration in order to find the function roots
+    allowedIteration = int(-(log(ACCURACY / (domainEnd - domainStart)) / log(2))) + 1
     print('---------- Bisection Method ----------')
     rootFinder(function, domainStart, domainEnd)
     print('\nCalculation Is Done, Check File "Calculation" For More Information')
