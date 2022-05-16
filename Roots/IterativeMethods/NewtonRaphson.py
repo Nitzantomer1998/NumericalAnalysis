4e37330cb3c551efe22e447faf6f04d355192be7
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-05-16 09:23:18 +0300

    Update appearance

--- Roots/IterativeMethods/NewtonRaphson.py
+++ Roots/IterativeMethods/NewtonRaphson.py
@@ -1,14 +1,14 @@
-# Finding The Roots In The Newton-Raphson Method
+# Finding Roots Using Newton-Raphson Method
 
 
-# Libraries
+# Libraries for getting the derivative of a function
 import sympy as sp
 from sympy.utilities.lambdify import lambdify
 
 # Libraries for calculation log
 from math import log
 
-# Global Variable To Set The Solution Accuracy
+# Global variable to set the accuracy of the solution
 ACCURACY = 0.00001
 
 
@@ -33,7 +33,7 @@ def rootFinder(f, startAt, endAt, maxIteration):
     # Divide our function domain range into multiply domains with 0.1 range, then search for each one of them for a root
     while startAt < endAt:
 
-     # In case the root is in the domain edge
+        # In case the root is in the domain edge
         if f(startAt) == 0:
             printIntoFile(['None (Special case)', startAt], True)
             print(f'The root --> {startAt}    Iteration --> None (Special case)')
@@ -92,19 +92,57 @@ def newtonRaphsonMethod(f, g, currentX, maxIteration):
         currentX = nextX
 
     # In case we didn't find the root within the allowed amount of iteration, Print a fail message and end the program
+    printIntoFile([maxIteration, 'Failed To Find The Root, The Newton Raphson Method Is Not Suitable For This Function'], True)
     print('Failed To Find The Root, The Newton Raphson Method Is Not Suitable For This Function')
     exit()
 
 
+def printIntoFile(data, isFinal):
+    """
+    Printing the data content into a specified file
+
+    :param data: Data is a list representing the arguments
+    :param isFinal: If True, We print the root solution, Else we print the calculation
+    """
+    # Open file and save the sent data
+    with open('Calculation.txt', 'a+') as file:
+
+        # In case it's the solution
+        if isFinal:
+            file.write(f'\nRoot --> {data[1]}    Iteration --> {data[0]}\n')
+            file.write('--------------------------------------------------------------------------------------------\n')
+
+        else:
+            for i in range(len(data)):
+                file.write('{: ^25}'.format(data[i]))
+            file.write('\n')
+
+
+def resetFile():
+    """
+    Reset the calculation file
+
+    """
+    with open('Calculation.txt', 'w') as file:
+        file.write('------------------------------ Newton-Raphson Method ------------------------------\n')
+        file.write('{: ^25}{: ^25}{: ^25}{: ^25}'.format('Iteration', 'x', 'f(x)', "f'(x)") + '\n')
+
+
 # Our Program Driver
 if __name__ == "__main__":
+
+    # Reset the calculation file
+    resetFile()
+
+    # Function to solve
     x = sp.symbols('x')
     function = x ** 4 + x ** 3 - 3 * x ** 2
     domainStart = -3
     domainEnd = 2
-    
+
     # Variable to store the maximum iteration in order to find the function roots
     allowedIteration = int(-(log(ACCURACY / (domainEnd - domainStart)) / log(2))) + 1
 
     print('---------- Newton Raphson Method ----------')
-    rootFinder(function, domainStart, domainEnd)
+    rootFinder(function, domainStart, domainEnd, allowedIteration)
+    print('\nCalculation Is Done, Check File "Calculation" For More Information')
