520db9f1a26586844a08dfb1d96b91514e684585
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-05-16 09:18:04 +0300

    Update the methods, to the limited run time

--- Roots/IterativeMethods/NewtonRaphson.py
+++ Roots/IterativeMethods/NewtonRaphson.py
@@ -12,19 +12,20 @@ from math import log
 ACCURACY = 0.00001
 
 
-def rootFinder(f, startAt, endAt):
+def rootFinder(f, startAt, endAt, maxIteration):
     """
-    Method for getting the function Roots
+    Method for finding the function Roots
 
     :param f: Our function
-    :param startAt: The left domain of the function
-    :param endAt: The right domain of the function
+    :param startAt: Left domain of the function
+    :param endAt: Right domain of the function
+    :param maxIteration: The maximum iteration for finding the root
     """
-    # Variables to store our derivative function
+    # Variables to store the derivative function
     g = f.diff(x)
     h = g.diff(x)
 
-    # Making our function be able to get an X
+    # Activating the functions to be able to get an X
     f = lambdify(x, f)
     g = lambdify(x, g)
     h = lambdify(x, h)
@@ -32,42 +33,53 @@ def rootFinder(f, startAt, endAt):
     # Divide our function domain range into multiply domains with 0.1 range, then search for each one of them for a root
     while startAt < endAt:
 
-        # In case the function changes its sign (It means there's at least one root)
+ 
+
+        # In case the function changes its sign (Means there's at least one root)
         if f(startAt) * f(startAt + 0.1) < 0:
-            root, iteration = newtonRaphsonMethod(f, g, startAt + 0.05)
+            root, iteration = newtonRaphsonMethod(f, g, startAt + 0.05, maxIteration)
+            printIntoFile([iteration, root], True)
             print('The root --> ' + str(root) + '\tIteration --> ' + str(iteration))
 
         # In case the derivative function changes its sign (Mean there's a possibility for a root)
         if g(startAt) * g(startAt + 0.1) < 0:
 
-            # Getting a possible root (The return of the derivative function can be a Root or an Extreme point)
-            possibleRoot, iteration = newtonRaphsonMethod(g, h, startAt + 0.05)
+            # Getting a possibility for a root (Might be a Root or an Extreme point)
+            possibleRoot, iteration = newtonRaphsonMethod(g, h, startAt + 0.05, maxIteration)
 
-            # Checking the possible root is indeed a root
+            # In case we found a root
             if abs(f(possibleRoot)) < ACCURACY:
+                printIntoFile([iteration, possibleRoot], True)
                 print('The root --> ' + str(possibleRoot) + '\tIteration --> ' + str(iteration))
+
+            else:
+                printIntoFile([iteration, '"Failed" Found Extreme Point, Not A Root'], True)
 
         # Update our domain for the next iteration
         startAt = startAt + 0.1
 
 
-def newtonRaphsonMethod(f, g, currentX):
+def newtonRaphsonMethod(f, g, currentX, maxIteration):
     """
-    Finding the function f root
+    Finding the function root
 
     :param f: Our function
     :param g: The derivative function of f
     :param currentX: The value of the middle domain range of the function
+    :param maxIteration: The maximum iteration for finding the root
     :return: The root of the function if existed, else according failed message
     """
     # Search the root within the maximum allowed iteration
-    for i in range(100):
+    for i in range(maxIteration):
 
         # Variable to store the next X
         nextX = currentX - f(currentX) / g(currentX)
 
-        # In case we found our root, Return root and the iteration number
-        if abs(nextX - currentX) < ACCURACY:
+        # Save the calculation in the file
+        printIntoFile([i + 1, nextX, f(nextX), g(nextX)], False)
+
+        # In case we found our root, Return the root and the iteration number
+        if abs(f(nextX)) < ACCURACY:
             return int(nextX * 10 ** 5) / 10 ** 5, i + 1
 
         # Update the currentX to be the new one
