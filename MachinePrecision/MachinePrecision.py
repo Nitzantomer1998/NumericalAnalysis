6835863450423a6c30c93135e84f9b9263c752ed
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-05-17 17:38:46 +0300

    Fixing grammer

--- MachinePrecision/MachinePrecision.py
+++ MachinePrecision/MachinePrecision.py
@@ -1,67 +1,68 @@
 # Machine Precision Finder
 
 
-# Global Variable [Only Used To print the iteration number]
-PRINT_COUNTER = 1
-
-
-def printIntoFile(data, message, isFinal):
+def printIntoFile(data, isFinal):
     """
-    Printing the data and the message content into a specified file
+    Printing the data content into a specified file
 
-    :param data: Data is a list representing the calculation variables
-    :param message: Message is a String representing the data explanation
-    :param isFinal: Indicate if we sent our solution
+    :param data: Data is a list representing the arguments
+    :param isFinal: If True, We print the solution, Else we print the calculation
     """
-    # Our Global Variable To Count The Iteration Number
-    global PRINT_COUNTER
+    # Open file and save the sent data
+    with open('Calculation.txt', 'a+') as file:
 
-    # In Case We Are Running A Calculation, It will create a new file with the method name
-    if PRINT_COUNTER == 1:
-        file = open('MP_Calculation.txt', 'w')
-        file.write('------------------------------ Machine Precision Method ------------------------------\n')
-        file.write('{: ^22}'.format('Iteration') + 'Calculation\n')
-        file.close()
-
-    # Open the file and save the data
-    with open('MP_Calculation.txt', 'a+') as file:
-
+        # In case it's the solution
         if isFinal:
-            # Saving the Solution
-            file.write(f'{message}{data}')
+            file.write(f'{data}')
 
         else:
-            # Saving the Calculation
-            if PRINT_COUNTER > 0:
-                for i in range(len(data)):
-                    if i == 0:
-                        file.write('{: ^22}'.format(f'({PRINT_COUNTER})'))
+            for i in range(len(data)):
+                if i == 0:
+                    file.write('{: ^22}'.format(f'({data[0]})'))
 
+                else:
                     file.write(f'{data[i]} ')
-                file.write('\n')
+            file.write('\n')
 
-        # Increase Our Global Iteration Counter Variable
-        PRINT_COUNTER = PRINT_COUNTER + 1
+
+def resetFile():
+    """
+    Reset the calculation file
+
+    """
+    with open('Calculation.txt', 'w') as file:
+        file.write('------------------------------ Machine Precision Method ------------------------------\n')
+        file.write('{: ^22}'.format('Iteration') + 'Calculation\n')
 
 
 def machinePrecisionMethod():
+    """
+    Finding the machine precision
 
-    # Variable
-    ourEpsilon = 1
+    """
+    # Variable to store our machine precision
+    ourPrecision = 1
 
-    # Loop To Find The Machine Precision
-    print("[Machine Precision Finder]")
-    while 1.0 + (ourEpsilon / 2) > 1.0:
-        printIntoFile([ourEpsilon, '\\', 2, '=', ourEpsilon / 2], None, False)
-        ourEpsilon = ourEpsilon / 2
+    # Variable to store the amount of iteration
+    loopCounter = 1
 
-    # Your Own Machine Precision
-    printIntoFile(ourEpsilon, '\nYour Epsilon Machine --> ', True)
-    print(f'Your Epsilon Is --> {ourEpsilon}')
+    # Loop for finding the Machine Precision
+    while 1.0 + (ourPrecision / 2) > 1.0:
+        printIntoFile([loopCounter, ourPrecision, '\\', 2, '=', ourPrecision / 2], False)
+        ourPrecision = ourPrecision / 2
+        loopCounter = loopCounter + 1
+
+    # Your Machine Precision
+    printIntoFile(f'\nYour Epsilon Machine --> {ourPrecision}', True)
+    print(f'Your Epsilon Is --> {ourPrecision}')
 
 
 # Our Program Driver
 if __name__ == "__main__":
+
+    # Reset the calculation file
+    resetFile()
+
+    print('---------- Machine Precision Method ----------')
     machinePrecisionMethod()
-    print('Calculation Is Done, Check File "MP_Calculation" For More Information')
-    
+    print('\nCalculation Is Done, Check File "Calculation" For More Information')
