2a96219ddcd28a81d05d6f16b6c717bf7fe8a415
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-05-31 17:30:54 +0300

    Adding python file for machine precision finder

---
+++ MachinePrecision/MachinePrecision.py
@@ -0,0 +1,21 @@
+# Machine Precision Finder
+
+
+def MachinePrecision():
+    """
+    Finding the machine precision
+
+    """
+    # Variable to store our machine precision
+    ourPrecision = 1
+
+    # Variable to store the amount of iteration
+    loopCounter = 1
+
+    # Loop for finding the Machine Precision
+    while 1.0 + (ourPrecision / 2) > 1.0:
+        ourPrecision = ourPrecision / 2
+        loopCounter = loopCounter + 1
+
+    # Your Machine Precision
+    print(f'Your Machine Accuracy --> {ourPrecision}')
