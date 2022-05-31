b773272f99f2905610e7f6ff7b6d6801355ba05f
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-05-31 17:34:37 +0300

    Adding file functions for saving the calculation

--- MachinePrecision/MachinePrecision.py
+++ MachinePrecision/MachinePrecision.py
@@ -14,11 +14,48 @@ def MachinePrecision():
 
     # Loop for finding the Machine Precision
     while 1.0 + (ourPrecision / 2) > 1.0:
+        printIntoFile([loopCounter, ourPrecision, '\\', 2, '=', ourPrecision / 2], None)
         ourPrecision = ourPrecision / 2
         loopCounter = loopCounter + 1
 
     # Your Machine Precision
+    printIntoFile(None, f'\nYour Machine Accuracy --> {ourPrecision}')
     print(f'Your Machine Accuracy --> {ourPrecision}')
+
+
+def printIntoFile(data, message):
+    """
+    Printing the content into a specified file
+
+    :param data: Data is a list representing matrix
+    :param message: Message is a string representing a message
+    """
+    # Open file and save the sent data
+    with open('Calculation.txt', 'a+') as file:
+
+        # In case we sent a message
+        if message:
+            file.write(message)
+
+        # In case we sent a data
+        if data:
+            for i in range(len(data)):
+                if i == 0:
+                    file.write('{: ^25}'.format(f'({data[0]})'))
+
+                else:
+                    file.write(f'{data[i]} ')
+            file.write('\n')
+
+
+def resetFile():
+    """
+    Reset the calculation file
+
+    """
+    with open('Calculation.txt', 'w') as file:
+        file.write('------------------------------- Machine Precision Method -------------------------------\n')
+        file.write('{: ^25}Calculation\n'.format('Iteration'))
 
 
 # Our Program Driver
@@ -26,3 +63,4 @@ if __name__ == "__main__":
 
     print('---------- Machine Precision Method ----------')
     MachinePrecision()
+
