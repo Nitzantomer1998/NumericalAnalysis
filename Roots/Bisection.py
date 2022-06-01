574bae1a0ebd1d83f64f56314e408d1667a97352
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-06-01 08:02:30 +0300

    Adding file functions for saving the calculation

--- Roots/Bisection.py
+++ Roots/Bisection.py
@@ -1,13 +1,13 @@
 # Finding Roots Using Bisection Method
 
+
 # Libraries for getting the derivative of a function
 import sympy as sp
 from sympy.utilities.lambdify import lambdify
 
-# Libraries for calculation log
 from math import log
 
-
+# Global variable to set the accuracy of the solution
 ACCURACY = 0.00001
 
 
@@ -32,6 +32,7 @@ def rootFinder(f, startAt, endAt, maxIteration):
 
         # In case the root is in the domain edge
         if f(startAt) == 0:
+            printIntoFile(None, f'The root --> {startAt}    Iteration --> None (Special case)')
             print(f'The root --> {startAt}    Iteration --> None (Special case)')
 
             startAt = startAt + 0.1
@@ -40,6 +41,7 @@ def rootFinder(f, startAt, endAt, maxIteration):
         # In case the function changes its sign (Means there's at least one root)
         if f(startAt) * f(startAt + 0.1) < 0:
             root, iteration = Bisection(f, startAt, startAt + 0.1, maxIteration)
+            printIntoFile(None, f'Root --> {root}    Iteration --> {iteration}')
             print(f'The root --> {root}    Iteration --> {iteration}')
 
         # In case the derivative function changes its sign (Mean there's a possibility for a root)
@@ -50,7 +52,11 @@ def rootFinder(f, startAt, endAt, maxIteration):
 
             # In case we found a root
             if abs(f(possibleRoot)) == 0:
+                printIntoFile(None, f'Root --> {possibleRoot}    Iteration --> {iteration}')
                 print(f'The root --> {possibleRoot}    Iteration --> {iteration}')
+
+            else:
+                printIntoFile(None, 'Failed found extreme point, Not a root')
 
         # Update our domain for the next iteration
         startAt = startAt + 0.1
@@ -72,6 +78,9 @@ def Bisection(f, leftDomain, rightDomain, maxIteration):
         # Variable to store the middle of the current function domain
         middle = leftDomain + (rightDomain - leftDomain) / 2
 
+        # Save the calculation in the file
+        printIntoFile([i + 1, middle, f(middle)], None)
+
         # In case we found our root, Return the root and the iteration number
         if abs(f(middle)) < ACCURACY:
             return int(middle * 10 ** 5) / 10 ** 5, i + 1
@@ -85,11 +94,48 @@ def Bisection(f, leftDomain, rightDomain, maxIteration):
             leftDomain = middle
 
     # In case we didn't find the root within the allowed amount of iteration, Print a fail message and end the program
+    printIntoFile(None, "Failed to find the root, Bisection Method isn't suitable")
     print("Failed to find the root, Bisection Method isn't suitable")
+
+
+def printIntoFile(data, message):
+    """
+    Printing the content into a specified file
+
+    :param data: Data is a list representing matrix
+    :param message: Message is a string representing a message
+    """
+    # Open file and save the sent content
+    with open('Calculation.txt', 'a+') as file:
+
+        # In case we sent a message
+        if message:
+            file.write('\n{: ^25}\n'.format(message))
+            file.write('--------------------------------------------------------------------------------------------\n')
+
+        # In case we sent a data
+        if data:
+            for i in range(len(data)):
+                file.write('{: ^25}'.format(float(data[i])))
+
+            file.write('\n')
+
+
+def resetFile():
+    """
+    Reset the calculation file
+
+    """
+    with open('Calculation.txt', 'w') as file:
+        file.write('------------------------------- Bisection Method -------------------------------\n')
+        file.write('{: ^25}{: ^25}{: ^25}\n'.format('Iteration', 'x', 'f(x)'))
 
 
 # Our Program Driver
 if __name__ == "__main__":
+
+    # Reset the calculation file
+    resetFile()
 
     # Function to solve
     x = sp.symbols('x')
@@ -103,4 +149,4 @@ if __name__ == "__main__":
     # Running the program
     print('---------- Bisection Method ----------')
     rootFinder(function, domainStart, domainEnd, allowedIteration)
-
+    print('\n\nCalculation Is Done, Check File "Calculation" For More Information')
