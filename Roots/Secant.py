40ca0633f9ad31ab9694bff1ce26c966c46f2e81
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-06-03 08:25:24 +0300

    Adding file functions for saving the calculation

--- Roots/Secant.py
+++ Roots/Secant.py
@@ -1,4 +1,6 @@
 # Finding Roots Using Secant Method
+
+
 # Libraries for getting the derivative of a function
 import sympy as sp
 from sympy.utilities.lambdify import lambdify
@@ -8,6 +10,8 @@ from math import log
 
 # Global variable to set the accuracy of the solution
 ACCURACY = 0.00001
+
+
 def rootFinder(f, startAt, endAt, maxIteration):
     """
     Method for finding the function Roots
@@ -29,6 +33,7 @@ def rootFinder(f, startAt, endAt, maxIteration):
 
         # In case the root is in the domain edge
         if f(startAt) == 0:
+            printIntoFile(None, f'The root --> {startAt}    Iteration --> None (Special case)')
             print(f'The root --> {startAt}    Iteration --> None (Special case)')
 
             startAt = startAt + 0.1
@@ -37,6 +42,7 @@ def rootFinder(f, startAt, endAt, maxIteration):
         # In case the function changes its sign (Means there's at least one root)
         if f(startAt) * f(startAt + 0.1) < 0:
             root, iteration = Secant(f, startAt, startAt + 0.1, maxIteration)
+            printIntoFile(None, f'The root --> {root}    Iteration --> {iteration}')
             print(f'The root --> {root}    Iteration --> {iteration}')
 
         # In case the derivative function changes its sign (Mean there's a possibility for a root)
@@ -46,8 +52,12 @@ def rootFinder(f, startAt, endAt, maxIteration):
             possibleRoot, iteration = Secant(g, startAt, startAt + 0.1, maxIteration)
 
             # In case we found a root
-            if abs(f(possibleRoot)) < 0.0001:
+            if abs(f(possibleRoot)) < ACCURACY:
+                printIntoFile(None, f'The root --> {possibleRoot}    Iteration --> {iteration}')
                 print(f'The root --> {possibleRoot}    Iteration --> {iteration}')
+
+            else:
+                printIntoFile(None, 'Failed found extreme point, Not a root')
 
         # Update our domain for the next iteration
         startAt = startAt + 0.1
@@ -69,8 +79,11 @@ def Secant(f, previewX, currentX, maxIteration):
         # Variable to store the next X
         nextX = (previewX * f(currentX) - currentX * f(previewX)) / (f(currentX) - f(previewX))
 
+        # Save the calculation in the file
+        printIntoFile([i + 1, previewX, nextX, f(nextX)], None)
+
         # In case we found our root, Return the root and the iteration number
-        if abs(f(nextX)) < 0.0001:
+        if abs(f(nextX)) < ACCURACY:
             return int(nextX * 10 ** 5) / 10 ** 5, i + 1
 
         # Update the previewX to be the currentX
@@ -80,21 +93,55 @@ def Secant(f, previewX, currentX, maxIteration):
         currentX = nextX
 
     # In case we didn't find the root within the allowed amount iteration, Print fail message and shut down the program
+    printIntoFile(None, "Failed to find the root, Secant Method isn't suitable")
     print("Failed to find the root, Secant Method isn't suitable")
+
+
+def printIntoFile(data, message):
+    """
+    Printing the content into a specified file
+
+    :param data: Data is a list representing matrix
+    :param message: Message is a string representing a message
+    """
+    # Open file and save the sent content
+    with open('Calculation.txt', 'a+') as file:
+
+        # In case we sent a message
+        if message:
+            file.write('\n{: ^25}\n'.format(message))
+            file.write('--------------------------------------------------------------------------------------------\n')
+
+        # In case we sent a data
+        if data:
+            for i in range(len(data)):
+                file.write('{: ^25}'.format(float(data[i])))
+
+            file.write('\n')
+
+
+def resetFile():
+    """
+    Reset the calculation file
+
+    """
+    with open('Calculation.txt', 'w') as file:
+        file.write('------------------------------- Secant Method -------------------------------\n')
+        file.write('{: ^25}{: ^25}{: ^25}{: ^25}\n'.format('Iteration', 'previewX', 'nextX', "f(nextX)"))
 
 
 # Our Program Driver
 if __name__ == "__main__":
 
-    # Input section
+    resetFile()
+
     x = sp.symbols('x')
     function = x ** 4 + x ** 3 - 3 * x ** 2
     domainStart = -3
     domainEnd = 2
 
-    # Variable to store the maximum iteration in order to find the function roots
     allowedIteration = int(-(log(ACCURACY / (domainEnd - domainStart)) / log(2))) + 1
 
-    # Running the program
     print('---------- Secant Method ----------')
     rootFinder(function, domainStart, domainEnd, allowedIteration)
+    print('\n\nCalculation Is Done, Check File "Calculation" For More Information')
