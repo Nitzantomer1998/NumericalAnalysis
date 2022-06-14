35c273de9286a2b3587b22d90f2b4f8ea2ae19f0
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-06-14 08:45:50 +0300

    Adjusting notes

--- Approximation/Interpolation/Polynomial.py
+++ Approximation/Interpolation/Polynomial.py
@@ -1,3 +1,6 @@
+# Finding Point Approximation Using Polynomial Method
+
+
 def Polynomial(pointsList, xToFind):
     """
     Method for finding approximation of the wanted point
@@ -5,28 +8,36 @@ def Polynomial(pointsList, xToFind):
     :param pointsList: List of point represent the points on the graph
     :param xToFind: value on the axis X, that we are searching for
     """
+    # In case we can't perform interpolation
     if len(pointsList) < 2:
         print('Error, Interpolation demand minimum of two points')
         return
 
+    # In case we can't perform interpolation
     if xToFind < pointsList[0][0] or xToFind > pointsList[-1][0]:
         print('The wanted point is not suitable for interpolation method')
         return
 
+    # Creating a Linear Equation from out List of points
     matrix = [[pointsList[row][0] ** col for col in range(len(pointsList))] for row in range(len(pointsList))]
     vectorB = [[pointsList[row][1] for _ in range(1)] for row in range(len(pointsList))]
 
+    # The solution of the Linear Equation
     vectorSolution = LU(matrix, vectorB)
 
+    # In case the equation system solution failed
     if any(vectorSolution) is None:
         print('Error, The equation system solver failed')
         return
 
+    # The Y approximation of the point x
     yApproximation = 0
 
+    # Loop to find Y approximation
     for i in range(len(vectorSolution)):
         yApproximation = yApproximation + vectorSolution[i][0] * (xToFind ** i)
 
+    # The point approximation
     print(f'Point Approximation --> ({xToFind}, {int(yApproximation * 10 ** 5) / 10 ** 5})')
 
 
@@ -37,25 +48,33 @@ def LU(originMatrix, originVectorB):
     :param originMatrix: NxN Matrix
     :param originVectorB: Nx1 Vector
     """
+    # Check if the matrix is Quadratic matrix, and check if the vector is in appropriate size
     if len(originMatrix) == len(originMatrix[0]) and len(originVectorB) == len(originMatrix) and len(originVectorB[0]) == 1:
 
         # In case the matrix has one solution
         if determinantMatrix(originMatrix):
 
+            # Organize the matrix pivots
             originMatrix, originVectorB = organizeMatrix(originMatrix, originVectorB)
 
+            # Getting the Lower, and Upper matrices of our equation system
             upperMatrix, lowerMatrix = findLU(originMatrix)
 
+            # Solve Ly = B
             vectorSolutionY = forwardSubstitution(lowerMatrix, originVectorB)
 
+            # Solve Ux = y (Getting the equation system solution)
             vectorSolutionX = finalSolution(originMatrix, originVectorB, backSubstitution(upperMatrix, vectorSolutionY))
 
+            # Returning the equation system final solution
             return vectorSolutionX
 
+        # According message In case there is more or less than one solution
         else:
             print('This is a Singular matrix')
             return None
 
+    # In case the input equation system isn't meet the demands
     else:
         print("The input equation system isn't match")
         return None
@@ -69,14 +88,19 @@ def organizeMatrix(originMatrix, originVectorB):
     :param originVectorB: Nx1 vector
     :return: The updated equation system
     """
+    # Loop to get the highest pivots possible
     for i in range(len(originMatrix)):
 
+        # Variable to store the highest value for the pivot
         maxPivot = abs(originMatrix[i][i])
 
+        # Variable to store the new pivot row
         pivotRow = -1
 
+        # Searching the highest potential Pivot for originMatrix[i][i]
         for j in range(i + 1, len(originMatrix)):
 
+            # In case there's a higher pivot (on the Column[i])
             if abs(originMatrix[j][i]) > maxPivot:
                 maxPivot = abs(originMatrix[j][i])
                 pivotRow = j
@@ -86,6 +110,7 @@ def organizeMatrix(originMatrix, originVectorB):
             originVectorB[i], originVectorB[pivotRow] = originVectorB[pivotRow], originVectorB[i]
             originMatrix[i], originMatrix[pivotRow] = originMatrix[pivotRow], originMatrix[i]
 
+    # Return the updated equation system
     return originMatrix, originVectorB
 
 
@@ -96,14 +121,17 @@ def findLU(upperMatrix):
     :param upperMatrix: NxN matrix of the equation system
     :return: Upper matrix, and Lower matrix
     """
+    # Initialize Lower Matrix into an Identity matrix
     lowerMatrix = [[1 if row == col else 0 for col in range(len(upperMatrix))] for row in range(len(upperMatrix))]
 
+    # Solving matrix into an Upper matrix, and Lower matrix
     for i in range(len(upperMatrix)):
         for j in range(i + 1, len(upperMatrix)):
             if upperMatrix[j][i] != 0:
                 lowerMatrix[j][i] = upperMatrix[j][i] / upperMatrix[i][i]
                 upperMatrix = multiplyMatrix(initElementaryMatrix(len(upperMatrix), j, i, - upperMatrix[j][i] / upperMatrix[i][i]), upperMatrix)
 
+    # Return Upper matrix, and Lower matrix
     return upperMatrix, lowerMatrix
 
 
@@ -115,14 +143,17 @@ def forwardSubstitution(lowerMatrix, vectorB):
     :param vectorB: Nx1 vector B
     :return: Nx1 vector solution
     """
+    # Initialize vectorY
     vectorY = [[0 for _ in range(1)] for _ in range(len(lowerMatrix))]
 
+    # Solve Ly = B
     for i in range(len(lowerMatrix)):
         vectorY[i][0] = vectorB[i][0]
         for j in range(i):
             vectorY[i][0] = vectorY[i][0] - lowerMatrix[i][j] * vectorY[j][0]
         vectorY[i][0] = vectorY[i][0] / lowerMatrix[i][i]
 
+    # Return vector solution
     return vectorY
 
 
@@ -134,15 +165,18 @@ def backSubstitution(upperMatrix, vectorY):
     :param vectorY: Nx1 vector Y
     :return: Nx1 vector solution
     """
+    # Initialize vectorX
     vectorX = [[0 for _ in range(1)] for _ in range(len(upperMatrix))]
     vectorX[-1][0] = vectorY[-1][0] / upperMatrix[-1][-1]
 
+    # Solve Ux = y
     for i in range(len(upperMatrix) - 2, -1, -1):
         rowSum = vectorY[i][0]
         for j in range(i + 1, len(upperMatrix)):
             rowSum = rowSum - upperMatrix[i][j] * vectorX[j][0]
         vectorX[i][0] = rowSum / upperMatrix[i][i]
 
+    # Return vector solution
     return vectorX
 
 
@@ -156,14 +190,17 @@ def finalSolution(originMatrix, originVectorB, vectorSolution):
     :param vectorSolution: Nx1 vector semi solution (not surly accurate)
     :return: Nx1 vector, the precise equation system solution
     """
+    # Solve r = Ax0 - b (Vector r represent the accuracy of the solution we found)
     vectorR = multiplyMatrix(originMatrix, vectorSolution)
     for i in range(len(vectorR)):
         vectorR[i][0] = vectorR[i][0] - originVectorB[i][0]
 
+    # Update to the correct solution
     for i in range(len(vectorSolution)):
         if abs(vectorSolution[i][0] - round(vectorSolution[i][0])) <= max(1e-09 * max(abs(vectorSolution[i][0]), abs(round(vectorSolution[i][0]))), 0):
             vectorSolution[i][0] = round(vectorSolution[i][0])
 
+    # Return the final solution of the equation system
     return vectorSolution
 
 
@@ -175,13 +212,16 @@ def multiplyMatrix(matrixA, matrixB):
     :param matrixB: NxM Matrix
     :return: NxM matrix
     """
+    # Initialize NxM matrix filled with zero's
     matrixC = [[0] * len(matrixB[0]) for _ in range(len(matrixA))]
 
+    # Multiply the two matrices and store the outcome in matrixC
     for i in range(len(matrixA)):
         for j in range(len(matrixB[0])):
             for k in range(len(matrixB)):
                 matrixC[i][j] = matrixC[i][j] + matrixA[i][k] * matrixB[k][j]
 
+    # Return the outcome matrix
     return matrixC
 
 
@@ -195,9 +235,11 @@ def initElementaryMatrix(size, row, col, value):
     :param value: Value parameter
     :return: Return the elementary matrix
     """
+    # Initialize the desire elementary matrix
     elementaryMatrix = [[1 if row == col else 0 for col in range(size)] for row in range(size)]
     elementaryMatrix[row][col] = value
 
+    # Return the elementary matrix
     return elementaryMatrix
 
 
@@ -208,30 +250,37 @@ def determinantMatrix(matrix):
     :param matrix: NxN Matrix
     :return: Matrix determinant
     """
+    # Simple case, The matrix size is 2x2
     if len(matrix) == 2:
         value = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
         return value
 
+    # Initialize our sum variable
     determinantSum = 0
 
+    # Loop to traverse each column of the matrix
     for current_column in range(len(matrix)):
         sign = (-1) ** current_column
 
+        # Calling the function recursively to get determinant value of sub matrix obtained
         determinant_sub = determinantMatrix(
             [row[: current_column] + row[current_column + 1:] for row in (matrix[: 0] + matrix[0 + 1:])])
 
+        # Adding the calculated determinant value of particular column matrix to total the determinantSum
         determinantSum = determinantSum + (sign * matrix[0][current_column] * determinant_sub)
 
     # Returning the final Sum
     return determinantSum
 
 
+# Our Program Driver
 if __name__ == "__main__":
 
+    # Graph of points (Got to be in rising X)
     graphPoints = [[0, 0], [1, 0.8415], [2, 0.9093], [3, 0.1411], [4, -0.7568], [5, -0.9589], [6, -0.2794]]
 
+    # The X value of the wanted point approximation
     xValue = 2.5
 
     print('---------- Polynomial Method ----------')
     Polynomial(graphPoints, xValue)
-    
