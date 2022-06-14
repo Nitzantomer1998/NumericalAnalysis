1a9157f8e72d5ebcc614cb344ca4b23316b80d06
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-06-14 08:43:01 +0300

    Adding main function

--- Approximation/Interpolation/Polynomial.py
+++ Approximation/Interpolation/Polynomial.py
@@ -224,3 +224,14 @@ def determinantMatrix(matrix):
 
     # Returning the final Sum
     return determinantSum
+
+
+if __name__ == "__main__":
+
+    graphPoints = [[0, 0], [1, 0.8415], [2, 0.9093], [3, 0.1411], [4, -0.7568], [5, -0.9589], [6, -0.2794]]
+
+    xValue = 2.5
+
+    print('---------- Polynomial Method ----------')
+    Polynomial(graphPoints, xValue)
+    
