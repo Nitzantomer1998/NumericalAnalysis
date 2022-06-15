e2646698ea971bed0cf1d3a0055206cc08de847a
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-06-15 09:47:43 +0300

    Adding main function

--- Approximation/Interpolation/Lagrange.py
+++ Approximation/Interpolation/Lagrange.py
@@ -27,3 +27,12 @@ def Lagrange(pointsList, xToFind):
         yToFind = yToFind + L * pointsList[i][1]
 
     print(f'Point Approximation --> ({xToFind}, {int(yToFind * 10 ** 5) / 10 ** 5})')
+
+
+if __name__ == "__main__":
+
+    graphPoints = [[0, 0], [1, 0.8415], [2, 0.9093], [3, 0.1411], [4, -0.7568], [5, -0.9589], [6, -0.2794]]
+    xValue = 2.5
+
+    print('---------- Lagrange Method ----------')
+    Lagrange(graphPoints, xValue)
