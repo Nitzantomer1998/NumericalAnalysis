6b14cc6de790d6167b709d6debf598ffb648035b
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-06-17 18:10:09 +0300

    Adding main function

--- Approximation/Interpolation/NaturalCubicSpline.py
+++ Approximation/Interpolation/NaturalCubicSpline.py
@@ -220,5 +220,12 @@ def determinantMatrix(matrix):
     return determinantSum
 
 
+if __name__ == "__main__":
+
+    graphPoints = [[0, 0], [1, 0.8415], [2, 0.9093], [3, 0.1411], [4, -0.7568], [5, -0.9589], [6, -0.2794]]
+    xValue = 2.5
+
+    print('---------- Natural Cubic Spline Method ----------')
+    NaturalCubicSpline(graphPoints, xValue)
 
 
