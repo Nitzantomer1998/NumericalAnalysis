8ab84c823eaf178add3f23bd9cf4b1c3b711edef
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-06-26 21:25:29 +0300

    Adding function to find the max Y value

--- Integral/Trapezoidal.py
+++ Integral/Trapezoidal.py
@@ -7,27 +7,50 @@ def Trapezoidal(f, leftDomain, rightDomain, sectionAmount):
     :param rightDomain: Right domain of the function
     :param sectionAmount: The amount of section
     """
-    # Variable to store the highest possible section amount without losing information in the process
     sectionAmountLimit = math.sqrt(((rightDomain - leftDomain) ** 3 * MaxFunctionValue(f, leftDomain, rightDomain)) / (12 * ACCURACY))
 
-    # In case the user chose more section than we can perform without losing information
     if sectionAmount > sectionAmountLimit:
 
-        # Appropriate failed message
         print(f'You chose too many section, the limit in this case is {sectionAmountLimit}')
 
-    # Initiate the function to be able to calculate Y base on given x
     f = lambdify(x, f)
 
-    # Calculating step size
     h = (rightDomain - leftDomain) / sectionAmount
 
-    # Initialize the integral value
     integral = f(leftDomain) + f(rightDomain)
 
-    # Calculate the rest of the integral value
     for i in range(1, sectionAmount):
         integral = integral + 2 * f(leftDomain + i * h)
 
-    # Print the area value approximation
     return int(integral * h / 2 * 10 ** 5) / 10 ** 5
+
+
+def MaxFunctionValue(f, startAt, endAt):
+    """
+    Method for finding the highest point of a function
+
+    :param f: Our function
+    :param startAt: Left domain of the function
+    :param endAt: Right domain of the function
+    """
+    g = f.diff(x)
+
+    f = lambdify(x, f)
+    g = lambdify(x, g)
+
+    maxIteration = int(-(log(ACCURACY / (domainEnd - domainStart)) / log(2))) + 1
+
+    maxValue = max(f(startAt), f(endAt))
+
+    while startAt < endAt:
+
+        if g(startAt) * g(startAt + 0.1) < 0:
+
+            possiblePoint = Secant(g, startAt, startAt + 0.1, maxIteration)
+
+            if f(possiblePoint) > maxValue:
+                maxValue = f(possiblePoint)
+
+        startAt = startAt + 0.1
+
+    return maxValue
