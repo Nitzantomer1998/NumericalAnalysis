e0bfa8d959a4bf5c92717aac17c16955ae7ebade
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-04 10:58:44 +0300

    Adding Trapezoidal function
    Helping Rombreg calculation

--- Integration/Romberg.py
+++ Integration/Romberg.py
@@ -18,3 +18,23 @@ def Romberg(f, startAt, endAt, sectionAmount):
             R[i][j + 1] = (4 ** (j + 1) * R[i][j] - R[i - 1][j]) / (4 ** (j + 1) - 1)
 
     print(f'Sum of the area --> {int(R[-1][-1] * 10 ** 5) / 10 ** 5}')
+
+
+def Trapezoidal(f, startAt, endAt, sectionAmount):
+    """
+    Method for finding the locked Area of the function in the segment domain [startAt, endAt]
+
+    :param f: Our function
+    :param startAt: Left domain of the function
+    :param endAt: Right domain of the function
+    :param sectionAmount: The amount of section
+    :return: Interval value
+    """
+    h = abs(endAt - startAt) / sectionAmount
+
+    interval = f(startAt) + f(endAt)
+
+    for i in range(1, sectionAmount):
+        interval = interval + 2 * f(startAt + i * h)
+
+    return interval * h / 2
