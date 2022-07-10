7ae0508e1f5be8c7a350e0721c2c29217ec4c5e9
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-10 07:27:13 +0300

    Adjusting notes

--- Interpolation/Lagrange.py
+++ Interpolation/Lagrange.py
@@ -1,3 +1,6 @@
+# Finding Point Approximation Using Lagrange Method
+
+
 def Lagrange(pointsList, xToFind):
     """
     Method for finding startAt Point based on the x value
@@ -5,35 +8,45 @@ def Lagrange(pointsList, xToFind):
     :param pointsList: List of point represent the points on the graph
     :param xToFind: value on the axis X, that we are searching for
     """
+    # In case we can't perform interpolation
     if len(pointsList) < 2:
         print('Error, Interpolation demand minimum of two points')
         return
 
+    # In case we can't perform interpolation
     if xToFind < pointsList[0][0] or xToFind > pointsList[-1][0]:
         print('The wanted point is not suitable for interpolation method')
         return
 
+    # The Y approximation of the point x
     yToFind = 0
 
+    # Loop to find Y approximation
     for i in range(len(pointsList)):
 
+        # Calculate Li(x)
         L = 1
 
+        # Loop to calculate the Li(x) result
         for j in range(len(pointsList)):
 
             if i != j:
                 L = L * (xToFind - pointsList[j][0]) / (pointsList[i][0] - pointsList[j][0])
 
+        # Calculate the approximation of Y in the current I iteration
         yToFind = yToFind + L * pointsList[i][1]
 
     # The point approximation
     print(f'Point Approximation --> ({xToFind}, {int(yToFind * 10 ** 5) / 10 ** 5})')
 
 
+# Our Program Driver
 if __name__ == "__main__":
 
+    # Input section
     graphPoints = [[0, 0], [1, 0.8415], [2, 0.9093], [3, 0.1411], [4, -0.7568], [5, -0.9589], [6, -0.2794]]
     xValue = 5.5
 
+    # Running the program
     print('---------- Lagrange Method ----------')
     Lagrange(graphPoints, xValue)
