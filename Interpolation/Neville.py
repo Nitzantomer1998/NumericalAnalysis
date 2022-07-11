3ad54a03fbea5f932cfaf77bae5f56fca3fd3b6e
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-11 07:53:13 +0300

    Adding recursiveNeville function
    helping calculation function for neville

--- Interpolation/Neville.py
+++ Interpolation/Neville.py
@@ -14,3 +14,27 @@ def Neville(pointsList, xToFind):
         return
 
     print(f'Point Approximation --> ({xToFind}, {int(recursiveNeville(pointsList, xToFind, 0, len(pointsList) - 1) * 10 ** 5) / 10 ** 5})')
+
+
+def recursiveNeville(pointsList, xToFind, i, j):
+    """
+    Recursive method for finding startAt Point based on the x value
+
+    :param pointsList: List of point represent the points on the graph
+    :param xToFind: value on the axis X, that we are searching for
+    :param i: Index that represent Xi
+    :param j: Index that represent Xj
+    :return: The approximation of Y based on the X
+    """
+    if i == j:
+        return pointsList[i][1]
+
+    if P[i + 1][j] is None:
+        P[i + 1][j] = recursiveNeville(pointsList, xToFind, i + 1, j)
+
+    if P[i][j - 1] is None:
+        P[i][j - 1] = recursiveNeville(pointsList, xToFind, i, j - 1)
+
+    return ((xToFind - pointsList[i][0]) * P[i + 1][j] - (xToFind - pointsList[j][0]) * P[i][j - 1]) / (pointsList[j][0] - pointsList[i][0])
+
+
