104b9f6e601a31deec0fa2230e567908c31ebe3c
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-09-11 17:12:10 +0300

    Update "__main__" function name and variables

--- Interpolation/Linear.py
+++ Interpolation/Linear.py
@@ -28,13 +28,11 @@ def is_inserted_data_valid(points_list, x_to_find):
     return True
 
 
-# Our Program Driver
 if __name__ == "__main__":
 
-    # Input section
-    graphPoints = [[0, 0], [1, 0.8415], [2, 0.9093], [3, 0.1411], [4, -0.7568], [5, -0.9589], [6, -0.2794]]
-    xValue = 5.5
+    graph_points = [[0, 0], [1, 0.8415], [2, 0.9093], [3, 0.1411], [4, -0.7568], [5, -0.9589], [6, -0.2794]]
+    x_value = 5.5
 
-    # Running the program
-    print('---------- Linear Method ----------')
-    LinearInterpolation(graphPoints, xValue)
+    print('---------- Linear Interpolation Method ----------')
+    linear_interpolation_method(graph_points, x_value)
+
