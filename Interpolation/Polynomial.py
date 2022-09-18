74fe359491688b164a9dbd0f51c4c10e2177143e
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-09-18 09:35:40 +0300

    Update determinantMatrix function name and variables

--- Interpolation/Polynomial.py
+++ Interpolation/Polynomial.py
@@ -117,6 +117,24 @@ def find_final_solution(origin_matrix, origin_vector_b, vector_solution):
     return vector_solution
 
 
+def calculate_determinant(matrix):
+    
+    if len(matrix) == 2:
+        value = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
+        return value
+
+    determinant_sum = 0
+
+    for current_column in range(len(matrix)):
+        sign = (-1) ** current_column
+
+        determinant_sub = calculate_determinant([row[: current_column] + row[current_column + 1:] for row in (matrix[: 0] + matrix[0 + 1:])])
+
+        determinant_sum = determinant_sum + (sign * matrix[0][current_column] * determinant_sub)
+
+    return determinant_sum
+
+
 def multiplyMatrix(matrixA, matrixB):
     """
     Multiplying two matrices and return the outcome matrix
@@ -154,36 +172,6 @@ def initElementaryMatrix(size, row, col, value):
 
     # Return the elementary matrix
     return elementaryMatrix
-
-
-def determinantMatrix(matrix):
-    """
-    Calculate the matrix determinant and return the result
-
-    :param matrix: NxN Matrix
-    :return: Matrix determinant
-    """
-    # Simple case, The matrix size is 2x2
-    if len(matrix) == 2:
-        value = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
-        return value
-
-    # Initialize our sum variable
-    determinantSum = 0
-
-    # Loop to traverse each column of the matrix
-    for current_column in range(len(matrix)):
-        sign = (-1) ** current_column
-
-        # Calling the function recursively to get determinant value of sub matrix obtained
-        determinant_sub = determinantMatrix(
-            [row[: current_column] + row[current_column + 1:] for row in (matrix[: 0] + matrix[0 + 1:])])
-
-        # Adding the calculated determinant value of particular column matrix to total the determinantSum
-        determinantSum = determinantSum + (sign * matrix[0][current_column] * determinant_sub)
-
-    # Returning the final Sum
-    return determinantSum
 
 
 # Our Program Driver
