c87f98d1ef94f35fc1f9d50229543683f53b90f2
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-09-20 13:23:59 +0300

    Update "__main__" function name and variables

--- Interpolation/Lagrange.py
+++ Interpolation/Lagrange.py
@@ -43,13 +43,9 @@ def is_inserted_data_valid(points_list, x_to_find):
     return True
    
    
-# Our Program Driver
 if __name__ == "__main__":
+    graph_points = [[0, 0], [1, 0.8415], [2, 0.9093], [3, 0.1411], [4, -0.7568], [5, -0.9589], [6, -0.2794]]
+    x_value = 2.5
 
-    # Input section
-    graphPoints = [[0, 0], [1, 0.8415], [2, 0.9093], [3, 0.1411], [4, -0.7568], [5, -0.9589], [6, -0.2794]]
-    xValue = 5.5
-
-    # Running the program
     print('---------- Lagrange Method ----------')
-    Lagrange(graphPoints, xValue)
+    lagrange_interpolation_method(graph_points, x_value)
