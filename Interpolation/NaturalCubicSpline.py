5551a7a24f4d7c4ee8756f75c00ef78780e2c943
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-09-21 10:04:03 +0300

    Update "__main__" function name and variables

--- Interpolation/NaturalCubicSpline.py
+++ Interpolation/NaturalCubicSpline.py
@@ -235,13 +235,11 @@ def multiply_matrices(matrix_a, matrix_b):
     return matrix_c
 
 
-# Our Program Driver
 if __name__ == "__main__":
 
-    # Input section
-    graphPoints = [[0, 0], [1, 0.8415], [2, 0.9093], [3, 0.1411], [4, -0.7568], [5, -0.9589], [6, -0.2794]]
-    xValue = 2.5
+    graph_points = [[0, 0], [1, 0.8415], [2, 0.9093], [3, 0.1411], [4, -0.7568], [5, -0.9589], [6, -0.2794]]
+    x_value = 2.5
 
-    # Running the program
-    print('---------- Natural Cubic Spline Method ----------')
-    NaturalCubicSpline(graphPoints, xValue)
+    print('---------- Natural Cubic Spline Interpolation Method ----------')
+    natural_cubic_spline_interpolation_method(graph_points, x_value)
+
